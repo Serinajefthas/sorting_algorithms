30288eee3191306f2b02634930990a6cd6aea5a7
Serinajefthas <serina.jefthas@gmail.com>
2023-07-29 11:44:45 -0700

    Task 3 & task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,4 +6,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *curr, *temp;
 
+	if (list == NULL)
+		return;
+	curr = *list;
+	while (curr != NULL)
+	{
+		while (curr->next && (curr->n > (curr->next->n)))
+		{
+			temp = curr->next;
+			curr->next = (temp)->next;
+			(temp)->prev = curr->prev;
+			if (curr->prev != NULL)
+				curr->prev->next = temp;
+			if (temp->next != NULL)
+				temp->next->prev = curr;
+			curr->prev = temp;
+			temp->next = curr;
+			if (temp->prev != NULL)
+				curr = temp->prev;
+			else if (temp->prev == NULL)
+				*list = temp;
+			print_list(*list);
+		}
+		curr = curr->next;
+	}
+}
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -16,27 +16,29 @@ void quick_sort(int *array, size_t size)
  * @array: array to sort
  * @low: lowest value in the current subarray(ie not main array)
  * @high: highest value in current subarray(ie not main array)
+ * @size: size of array
  */
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
 	int pivot_index;
 
-	if (low < high)	
+	if (low < high)
 	{
 		pivot_index = partition(array, low, high, size);
 		/* sorts lower part of array < pivot */
 		quick_sort_recursive(array, low, pivot_index - 1, size);
 		/* sorts higher values of array that > pivot */
-		quick_sort_recursive(array, pivot_index - 1, high, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
 	}
 }
 
 /**
  * partition - partitions part of current array around pivot
  * @array: array of integers to sort
- * @low:
- * @high:
- * Returns: value of pivot?
+ * @low: low value left of pivot
+ * @high: high value right of pivot
+ * @size: size of array
+ * Return: value of pivot
  */
 int partition(int *array, int low, int high, size_t size)
 {
@@ -59,6 +61,6 @@ int partition(int *array, int low, int high, size_t size)
 	array[i] = array[high];
 	array[high] = temp;
 	print_array(array, size);
- 	return (i);
+	return (i);
 }
 
